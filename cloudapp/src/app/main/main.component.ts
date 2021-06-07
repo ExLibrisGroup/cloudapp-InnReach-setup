e09c33917360af74fd6fda22f8cbb7c9230d63f3
Liron Menda <Liron.Menda@exlibrisgroup.com>
2021-06-07 18:40:28 +0300

    Added alerts ,spinner , show log button + little refactoring

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import {
   CloudAppRestService, CloudAppEventsService, AlertService
 } from '@exlibris/exl-cloudapp-angular-lib';
+import { map, concatMap, tap } from 'rxjs/operators';
+import { forkJoin } from "rxjs";
 
 @Component({
   selector: 'app-main',
@@ -12,6 +14,9 @@ import {
 export class MainComponent implements OnInit {
 
   logString: string = "";
+  isLoading: boolean = false;
+  allowRun: boolean = false;
+
 
   private integrationProfileId: string;
 
@@ -21,7 +26,7 @@ export class MainComponent implements OnInit {
   constructor(private restService: CloudAppRestService,
     private eventsService: CloudAppEventsService,
     private http: HttpClient,
-    private alert: AlertService ) { }
+    private alert: AlertService) { }
 
   ngOnInit() {
     this.loadLocationCodes();
@@ -40,89 +45,83 @@ export class MainComponent implements OnInit {
       result.library.forEach((elementLib, index) => {
         var libCode = elementLib.code;
         this.restService.call('/conf/libraries/' + libCode + '/locations').subscribe(result => {
-            //console.log(result);
-            if (result.location) {
-              this.logString += "...found " + result.location.length + " locations in library: " + libCode + "\n";
-              result.location.forEach((elementHol, index) => {
-                var locCode = elementHol.code; var holname = elementHol.name;
-                //console.log(libCode +" > " + holCode);
-                if (this.allLocationCodes.includes(locCode)) {
-                  this.logString += "Warning: Duplicate location code: " + locCode + ". Skipping the one in library: " + libCode + "\n";
-                } else {
-                  this.allLocationCodes.push(locCode);
-                  this.locationsJsonArr.push( {"locationKey":locCode, "description":holname } );
-                }
+          //console.log(result);
+          if (result.location) {
+            this.logString += "...found " + result.location.length + " locations in library: " + libCode + "\n";
+            result.location.forEach((elementHol, index) => {
+              var locCode = elementHol.code; var holname = elementHol.name;
+              //console.log(libCode +" > " + holCode);
+              if (this.allLocationCodes.includes(locCode)) {
+                this.logString += "Warning: Duplicate location code: " + locCode + ". Skipping the one in library: " + libCode + "\n";
+              } else {
+                this.allLocationCodes.push(locCode);
+                this.locationsJsonArr.push({ "locationKey": locCode, "description": holname });
+              }
 
-              });
-            }
+            });
           }
+        }
         );
       });
     });
   }
 
   loadIntegrationProfileId() {
-    this.restService.call('/conf/integration-profiles?q=name~innreach&type=ILL_INTEGRATION').subscribe(result => 
-      { 
-        console.log("loadIntegrationProfileId(): ", result);
-        if (!result.integration_profile) {
-          this.logString += "Error: No ILL_INTEGRATION integration profile with innreach in its name detected!\n";
-          return;
-        }
-        if (result.integration_profile.length > 1) {
-          this.logString += "Warning: More than one ILL_INTEGRATION integration profile detected - using the first one, which has code: " +
-            result.integration_profile[0].code + "\n";
-        }
-
-        this.integrationProfileId = result.integration_profile[0].id;
-        this.logString += "integration-profile ID: " + this.integrationProfileId + "\n";
+    this.restService.call('/conf/integration-profiles?q=name~innreach&type=ILL_INTEGRATION').subscribe(result => {
+      console.log("loadIntegrationProfileId(): ", result);
+      if (!result.integration_profile) {
+        this.logString += "Error: No ILL_INTEGRATION integration profile with innreach in its name detected!\n";
+        this.alert.error("No Integration profile ID as been found");
+        return;
       }
+      if (result.integration_profile.length > 1) {
+        this.logString += "Warning: More than one ILL_INTEGRATION integration profile detected - using the first one, which has code: " +
+          result.integration_profile[0].code + "\n";
+      }
+
+      this.integrationProfileId = result.integration_profile[0].id;
+      this.logString += "integration-profile ID: " + this.integrationProfileId + "\n";
+
+      this.allowRun = !this.allowRun;
+
+    }
     );
   }
 
-  
-  callInnreach() {
+
+  callInnreach(e) {
+    console.log(e)
+    this.isLoading = true;
     this.logString += "Unique location codes: " + this.allLocationCodes + "\n";
 
     let locationsJson = { "locationList": this.locationsJsonArr };
     // tests: 
     // let locationsJson={"locationList":[{"locationKey":"amlc1", "description":"Alma North Branch"},{"locationKey":"amlc2", "description":"Alma South Branch"}]}
     // let locationsJson={"locationList":[{"locationKey":"amlc11","description":"Music Theses Suppressed"}]};
-
-    this.eventsService.getInitData().subscribe(
-      data => {
-        let url = data.urls['alma'] + "/view/innreachCloudApp";
-        console.log("getInitData (to get Alma's URL): ", url);
-
-        this.logString += "User: " + data.user.primaryId + "\n";
-        //console.log("App loading Alma's JWT...");
-        this.eventsService.getAuthToken().subscribe(
-          async jwt => {
-            this.logString += "Fetched token from Alma\nCalling https://rssandbox-api.iii.com/innreach/v2/contribution/locations ...\n";
-            this.logString += "Body:\n"+JSON.stringify(locationsJson)+"\n";
-            let authHeader = "Bearer "+ jwt;
-            console.log ("authHeader:",authHeader);
-            const headers = new HttpHeaders({'Authorization': authHeader, 'X-integrationProfileId': this.integrationProfileId});
-
-            await this.http.post<any>(url, locationsJson, { headers }) //.pipe(timeout(60*1000))
-            .toPromise().then(
-              response => {
-                console.log("rr");
-                console.log(response);
-                this.logString += "Response: " + JSON.stringify(response) + "\n";
-              }, error => {
-                console.log("er");
-                console.log(error);
-                this.logString += "Response: " + JSON.stringify(error.error) + "\n";
-              }
-            ).catch(e => {
-              console.log("ee");
-              console.log(e);
-              this.logString += "Response: "+ e + "\n";
-            });
-          }
-        )
+    forkJoin({ initData: this.eventsService.getInitData(), authToken: this.eventsService.getAuthToken() }).pipe(concatMap((data) => {
+      let url = data.initData.urls['alma'] + "/view/innreachCloudApp";
+      console.log("getInitData (to get Alma's URL): ", url);
+      this.logString += "User: " + data.initData.user.primaryId + "\n";
+      this.logString += "Fetched token from Alma\nCalling https://rssandbox-api.iii.com/innreach/v2/contribution/locations ...\n";
+      this.logString += "Body:\n" + JSON.stringify(locationsJson) + "\n";
+      let authHeader = "Bearer " + data.authToken;
+      console.log("authHeader:", authHeader);
+      const headers = new HttpHeaders({ 'Authorization': authHeader, 'X-integrationProfileId': this.integrationProfileId });
+      return this.http.post<any>(url, locationsJson, { headers })
+    })).subscribe({
+      next: response => {
+        console.log("rr");
+        console.log(response);
+        this.logString += "Response: " + JSON.stringify(response) + "\n";
+        this.alert.success("Done");
+        this.isLoading = false;
+      }, error: error => {
+        console.log("er");
+        console.log(error);
+        this.logString += "Response: " + JSON.stringify(error.error) + "\n";
+        this.alert.error(`Operation failed cause to ${error.message}`)
+        this.isLoading = false;
       }
-    );
+    });
   }
 }
